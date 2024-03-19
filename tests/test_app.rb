4aee0f53a6a7a9872def318cbb1ade5dc9d33f31
Dylan Sarikas <97060448+dylansarikas@users.noreply.github.com>
2024-03-19 19:56:23 +0000

    changes test

--- tests/test_app.rb
+++ tests/test_app.rb
@@ -2,18 +2,19 @@ require 'minitest/autorun'
 require './app.rb'
 
 class TestApp < Minitest::Test
+  def setup
+    @cookbook = Cookbook.new
+  end
 
   def test_add
-    cookbook = Cookbook.new
-    assert_equal "Pasta", cookbook.add("Pasta"), "Add Method Failed"
+    assert_equal "Pasta", @cookbook.add("Pasta"), "Add Method Failed"
   end
 
   def test_list
-    cookbook = Cookbook.new
-    cookbook.add("Pasta")
-    cookbook.add("Cake")
-    cookbook.add("Stew")
-    assert_equal 3, cookbook.list(), "List Method Failed"
+    @cookbook.add("Pasta")
+    @cookbook.add("Cake")
+    @cookbook.add("Stew")
+    assert_equal 3, @cookbook.list(), "List Method Failed"
   end
 
 end
